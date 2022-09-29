10b2957701925afd87636a5b9fc3e0035f5bcea3
Hisashi Takeda <tkdhss111@gmail.com>
2022-09-29 22:11:45 +0900

    Add files via upload

---
+++ class02/mklink_tex2pdfs_run_as_admin.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+echo %~dp0\tex2pdfs.sh %homedrive%%homepath%\AQUOS\Default_Folder\TIU\lectures\tex\tex2pdfs.sh
+mklink %~dp0\tex2pdfs.sh %homedrive%%homepath%\AQUOS\Default_Folder\TIU\lectures\tex\tex2pdfs.sh
+pause
